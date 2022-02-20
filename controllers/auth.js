469ed6cbf8f3b9ede333d16b79ef837713cab59f
robinl3680 <robinl3680@gmail.com>
2022-02-20 22:00:37 +0530

    sendgrid update

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key: process.env.SEND_GRID_KEY;
+        api_key: process.env.SEND_GRID_KEY
     }
 }));
 const JSWT = require('jsonwebtoken');
